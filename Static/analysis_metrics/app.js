c44945cda5593d711449dd133095e3fd41289d00
atjohnson1994 <atjohnson1994@gmail.com>
2024-04-02 12:59:38 -0600

    couple of edits

--- Static/analysis_metrics/app.js
+++ Static/analysis_metrics/app.js
@@ -26,24 +26,16 @@ function optionChanged(new_data) {
 // initizlize the graph for first ID
 function graph() {
     let x_values = []
-    let y_values = []
     for (i = 0; i < data.length; i++) {
         x_values.push(data[i]['acousticness'])
-        y_values.push(data[i]['popularity'])
     }
     console.log(x_values)
-    // console.log(y_values)
+
     // set data for graph
     let trace1 = {
         x: x_values,
         type: 'histogram'
     };
-    let trace2 = {
-        x: x_values,
-        y: y_values,
-        type: 'bar'
-    };
-    console.log(trace1)
     let layout = {
         xaxis: {
             title: 'Metric Value',
@@ -66,37 +58,19 @@ function graph() {
             t:40
         }
     }
-    let layout2 = {
-        yaxis: {
-            title: 'Popularity',
-            titlefont: 'Old Standard TT, serif',
-            size: 24,
-            color: 'grey'
-        },
-        height: 400,
-        width: 1000,
-        margin: {
-            b:80,
-            l:80,
-            r:80,
-            t:40
-        }
-    }
+
     // plot graph
     let plot_data = [trace1];
-    let plot2_data = [trace2];
     Plotly.newPlot('bar', plot_data, layout);
-    Plotly.newPlot('bar2', plot2_data, layout2);
+
 }
 
 
 // change plotly graph to new subject id
 function regraph(new_data) {
     let x_values = []
-    let y_values = []
     for (i = 0; i < data.length; i++) {
         x_values.push(data[i][`${new_data}`])
-        y_values.push(data[i]['popularity'])
     }
 
 
@@ -105,7 +79,6 @@ function regraph(new_data) {
 
     let plot_data = [];
     let trace1 = {};
-    let trace2 = {};
 
     // set data for graph
     trace1 = {
@@ -114,30 +87,12 @@ function regraph(new_data) {
 
     };
 
-    trace2 = {
-        x: x_values,
-        y: y_values,
-        type: 'bar'
-    }
-
-    const counter = {};
-    trace2.x.forEach(ele => {
-        if (counter[ele]) {
-            (counter[ele]) += 1;
-        } else {
-            (counter[ele]) = 1;
-        }
-    });
-    console.log(counter)
 
 
     // plot graph
     plot_data = [trace1];
-    plot2_data = [trace2];
 
     Plotly.restyle('bar', 'x', [plot_data[0]['x']]);
-    Plotly.restyle('bar2', 'x', [plot2_data[0]['x']]);
-    Plotly.restyle('bar2', 'y', [plot2_data[0]['y']]);
 }
 
 
