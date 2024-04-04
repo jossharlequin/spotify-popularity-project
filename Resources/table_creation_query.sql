aeb9c849b680d9e443edd70f97b5b0792f5d63a0
JosephNichols2000 <147287483+JosephNichols2000@users.noreply.github.com>
2024-04-04 17:27:56 -0600

    added sql table and data cleaning steps

---
+++ Resources/table_creation_query.sql
@@ -0,0 +1,16 @@
+CREATE TABLE spotify (
+	acousticness FLOAT,
+	danceability FLOAT,
+	energy FLOAT,
+	explicit BOOL,
+	instrumentalness FLOAT,
+	key INT,
+	liveness FLOAT,
+	loudness FLOAT,
+	mode BOOL,
+	popularity INT,
+	speechiness FLOAT,
+	tempo FLOAT,
+	valence FLOAT,
+	duration_seconds FLOAT
+ );
